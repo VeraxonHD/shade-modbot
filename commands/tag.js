cf9dcae39cc936f83f42f217e8a7bf3f02bd9424
VeraxonHD <toxicscopesgames@gmail.com>
2017-04-01 22:37:28 +0100

    Tags delete fixed
    Tags delete fixed
    Added db to to the repo

--- commands/tag.js
+++ commands/tag.js
@@ -4,10 +4,12 @@ var guild = message.guild
 
   if (args[0] == "create") {
     var commandname = args[1]
-    if(!guild.members.get(message.author).hasPermission("MANAGE_MESSAGES")){return react.noPermReact()}
+    if(!guild.members.get(message.author.id).hasPermission("MANAGE_MESSAGES")){return react.noPermReact()}
     sql.get(`SELECT * FROM tags WHERE commandname ='${commandname}'`).then(row => {
        if (!row) {
-         sql.run('INSERT INTO tags (commandname, action) VALUES (?, ?)', [commandname, args[2]]);
+         sql.run('INSERT INTO tags (commandname, action) VALUES (?, ?)', [commandname, args.slice(2).join(" ")]);
+         message.channel.sendMessage(`Eos \`Success\` - Command ${commandname} created!`)
+             .then(message=>message.react('✅'))
        } else {
          message.channel.sendMessage("Eos \`Error`\ - That command already exists!")
              .then(message=>message.react('❎'));
@@ -15,18 +17,25 @@ var guild = message.guild
      }).catch((err) => {
        console.error(err);
        sql.run('CREATE TABLE IF NOT EXISTS tags (commandname TEXT, action TEXT)').then(() => {
-         sql.run('INSERT INTO tags (commandname, action) VALUES (?, ?)', [commandname, args[2]]);
+         sql.run('INSERT INTO tags (commandname, action) VALUES (?, ?)', [commandname, args.slice(2).join(" ")]);
+
+         message.channel.sendMessage(`Eos \`Success\` - Command ${commandname} created!`)
+             .then(message=>message.react('✅'))
        });
      });
 
   }else if (args[0] == "delete") {
-    if(!guild.members.get(message.author).hasPermission("MANAGE_MESSAGES")){return react.noPermReact()}
+    if(!guild.members.get(message.author.id).hasPermission("MANAGE_MESSAGES")){return react.noPermReact()}
     var commandname = args[1]
     sql.get(`SELECT * FROM tags WHERE commandname = '${commandname}'`).then(row =>{
-      if (!row) return message.channel.sendMessage("Eos \`Error`\ - That command does not exist!")
-          .then(message=>message.react('❎'));
-
-      sql.run(`DELETE FROM tags WHERE commandname = ${row.commandname}`)})
+      if (!row){
+        message.channel.sendMessage("Eos \`Success`\ - That command does not exist!")
+          .then(message=>message.react('❎'))
+          return;
+      }
+      sql.run(`DELETE FROM tags WHERE commandname = "${row.commandname}"`)})
+      message.channel.sendMessage(`Eos \`Error\` - Command ${commandname} deleted`)
+          .then(message=>message.react('✅'))
 
     .catch((err) => {
       console.error(err)
