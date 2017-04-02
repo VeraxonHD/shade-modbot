8d2939b2100697d74223bb643d396f0b40139d51
VeraxonHD <toxicscopesgames@gmail.com>
2017-04-02 19:05:10 +0100

    Added to userinfo. Slightly broken as of today. Will fix tomorrow

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,25 +1,34 @@
 exports.run = (client, message, args, Discord) => {
+
   var username = message.mentions.users.first().username
   var userid = message.mentions.users.first().id
   var guild = message.guild
   var nickname = guild.members.get(userid).nickname
   var discrim = message.mentions.users.first().discriminator
+  var joined = guild.members.get(userid).joinedAt
+
 
   if(args.length < 1){
     message.reply("Eos \`Error`\ - You must identify a user to list.")
     .then(message=>message.react('❎'));
   }else{
-  const embed = new Discord.RichEmbed()
+  message.channel.fetchMessage(guild.members.get(userid).lastMessageID)
+      .then((lastseen) => {
+    var embed = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTimestamp(message.createdAt)
     .addField("User info for", username, true)
     .addField("Current Nickname", nickname, true)
     .addField("User ID", userid, true)
     .addField("Discriminator", discrim, true)
+    .addField("Last Seen", lastseen.createdAt, true)
+    .addField("Joined", joined, true)
     .setFooter(`Info on ${username} requested by ${message.author.username}`);
     //sends the embed
     message.channel.sendEmbed(
       embed,
     {disableEveryone: true })
+  })
+  .catch(console.log)
   }
 }
