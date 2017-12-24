884ba5d2353327b899318d2d13ab541ab1a68d4b
VeraxonHD <toxicscopesgames@gmail.com>
2017-12-24 14:37:46 +0000

    Fixed issue where event detects mute/deaf which is irrelevant

--- eos.js
+++ eos.js
@@ -110,29 +110,35 @@ client.on("voiceStateUpdate", (oldMember, newMember) => {
   var embed = new Discord.RichEmbed();
   var guild = oldMember.guild
   var user = newMember.nickname
-  var voicelogchannel = guild.channels.get(config[guild.id].voicelogchannelid)
-  if(!voicelogchannel){
-    voicelogchannel = guild.channels.get(config[guild.id].logchannelID)
-  }
 
-  if(!user){
-    user = newMember.user.username
-  }
 
-  if(!oldMember.voiceChannel){
-    embed.addField("User joined a voice channel", `${user} joined ${newMember.voiceChannel.name}.`, true)
-  }else if(!newMember.voiceChannel){
-    embed.addField("User disconnected from voice channels", `${user} left ${oldMember.voiceChannel.name}.`, true)
-  }else{
-    embed.setAuthor(`${user} changed voice channels.`)
-    embed.addField("Old channel", `${oldMember.voiceChannel.name}`, true)
-    embed.addField("New channel", `${newMember.voiceChannel.name}`, true)
-  }
+    var voicelogchannel = guild.channels.get(config[guild.id].voicelogchannelid)
+    if(!voicelogchannel){
+      voicelogchannel = guild.channels.get(config[guild.id].logchannelID)
+    }
 
-  embed.addField("User ID", newMember.id)
-  embed.setColor(newMember.guild.member(client.user).highestRole.color)
-  embed.setTimestamp(newMember.createdAt)
-  voicelogchannel.send({embed}).catch(console.log)
+    if(!user){
+      user = newMember.user.username
+    }
+
+    if(!oldMember.voiceChannel){
+      embed.addField("User joined a voice channel", `${user} joined ${newMember.voiceChannel.name}.`, true)
+    }else if(!newMember.voiceChannel){
+      embed.addField("User disconnected from voice channels", `${user} left ${oldMember.voiceChannel.name}.`, true)
+    }else{
+      embed.setAuthor(`${user} changed voice channels.`)
+      if((oldMember.mute == true) || (oldMember.deaf == true) || (newMember.mute == true) || (newMember.deaf == true)){
+        return;
+      }else{
+        embed.addField("Old channel", `${oldMember.voiceChannel.name}`, true)
+        embed.addField("New channel", `${newMember.voiceChannel.name}`, true)
+      }
+    }
+
+    embed.addField("User ID", newMember.id)
+    embed.setColor(newMember.guild.member(client.user).highestRole.color)
+    embed.setTimestamp(newMember.createdAt)
+    voicelogchannel.send({embed}).catch(console.log)
 })
 //Starboard message reaction
 client.on("messageReactionAdd", (messageReaction, user) =>{
